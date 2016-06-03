42f9d58da6f70260d9cef0109f3c86b5409ffdff
Andreas Thyrhaug <chessmachine75@gmail.com>
2016-06-03 16:59:51 +0200

    Improved the console verification.

--- ConsoleVerification/Program.cs
+++ ConsoleVerification/Program.cs
@@ -3,18 +3,58 @@
     using System;
     using Todo.Controllers;
 
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            while (true) {
-                Console.WriteLine("Add a task: ");
-                var input = Console.ReadLine();
-                var task = new Task(input);
-                Console.WriteLine("Description: " + task.Description);
-                Console.WriteLine("Due Date: " + task.DueDate);
-                Console.WriteLine();
+            TestDescriptionAndNoDueDate();
+            TestMayDueDate();
+
+            Console.ReadLine();
+        }
+
+        private static void TestDescriptionAndNoDueDate()
+        {
+            var input = "Pickup the groceries";
+            Console.WriteLine("Scenario: " + input);
+
+            var task = new Task(input);
+
+            var descriptionShouldBe = input;
+            DateTime? dueDateShouldBe = null;
+            var success = descriptionShouldBe == task.Description
+                          && dueDateShouldBe == task.DueDate;
+
+            var failureMessage = "Description: " + task.Description + " should be " + descriptionShouldBe
+                + Environment.NewLine
+                + "Due Date: " + task.DueDate + " should be " + dueDateShouldBe;
+
+            PrintOutcome(success, failureMessage);
+        }
+
+        private static void PrintOutcome(bool success, string failureMessage)
+        {
+            if (success) {
+                Console.WriteLine("SUCCESS");
             }
+            else {
+                Console.WriteLine("ERROR: ");
+                Console.WriteLine(failureMessage);
+            }
+            Console.WriteLine();
+        }
+
+        private static void TestMayDueDate()
+        {
+            var input = "Pickup the groceries may 5";
+            Console.WriteLine("Scenario: " + input);
+
+            var task = new Task(input);
+
+            var dueDateShouldBe = new DateTime(DateTime.Today.Year, 5, 5);
+            var success = dueDateShouldBe == task.DueDate;
+            var failureMessage = "Due Date: " + task.DueDate + " should be " + dueDateShouldBe;
+            PrintOutcome(success, failureMessage);
         }
     }
 }
